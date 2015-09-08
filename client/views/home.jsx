f09d6f51ad5d05e011c176e29aeeb70bc4f4d9d1
Manu <bionikspoon@gmail.com>
2015-09-07 23:58:16 -0500

    refactored connect decorator

--- client/views/home.jsx
+++ client/views/home.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import FilterPokemon from 'components/FilterPokemon';
 import PokemonList from 'components/PokemonList';
 import * as PokemonActions from 'actions/pokemon';
-import {bindActionCreators} from 'redux';
 
 
-@connect(state => ({
-  data: state.pokemon
-}))
+@connect(state => state.pokemon,
+         PokemonActions,
+         (data, actions, props) =>({data, actions, ...props})
+)
 export default class HomeView extends React.Component {
   static propTypes = {
-    dispatch: React.PropTypes.func.isRequired,
-    data: React.PropTypes.object.isRequired
+    data: React.PropTypes.object.isRequired,
+    actions: PropTypes.object.isRequired
   };
 
   constructor() {
@@ -21,18 +21,19 @@ export default class HomeView extends React.Component {
 
 
   render() {
-    const { pokemon, caughtPokemon  } = this.props.data;
-    const actions = bindActionCreators(PokemonActions, this.props.dispatch);
-
+    const { data, actions  } = this.props;
+    console.log(this.props);
     return (
       <section className='container text-center'>
         <div className="row">
           <div className="col-sm-8 col-sm-offset-2">
 
             <h1>Pokedex in Redux</h1>
-            <FilterPokemon actions={actions} /> <PokemonList pokemon={ pokemon }
-                                                             caughtPokemon={caughtPokemon}
-                                                             actions={actions} />
+            <FilterPokemon actions={actions} />
+
+            <PokemonList pokemon={ data.pokemon }
+                         caughtPokemon={data.caughtPokemon}
+                         actions={actions} />
           </div>
         </div>
       </section>
